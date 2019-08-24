32fa5c7b1a4a76592e8e62303f930226799864fc
kimbeomgyu <qjarb1994@gmail.com>
2019-08-24 21:23:11 +0900

    create event login

--- scripts/auth.js
+++ scripts/auth.js
@@ -15,10 +15,29 @@ signupForm.addEventListener('submit', e => {
   });
 });
 
+// logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', e => {
   e.preventDefault();
   auth.signOut().then(() => {
     console.log('user signed out');
+  });
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
